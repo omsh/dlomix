724ef58d243c22327926deb3bf23a6e6236bc1ff
ayla-s <evaaylaschroeder@gmail.com>
2025-02-05 11:00:38 +0000

    first raw draft of architecture translation from tf to torch for DominantChargeStatePredictorTorch

--- src/dlomix/models/chargestate_torch.py
+++ src/dlomix/models/chargestate_torch.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
+import torch.nn as nn
+from collections import OrderedDict
 
 from ..constants import ALPHABET_UNMOD
 from ..layers.attention import AttentionLayer
@@ -21,7 +22,7 @@ There are three task formulations, each with a corresponding model:
 """
 
 
-class DominantChargeStatePredictor(tf.keras.Model):
+class DominantChargeStatePredictorTorch(nn.Module):
     """
     Charge State Prediction Model for predicting the dominant charge state of a peptide sequence.
 
@@ -47,7 +48,7 @@ class DominantChargeStatePredictor(tf.keras.Model):
         regressor_layer_size=512,
         num_classes=6,
     ):
-        super(DominantChargeStatePredictor, self).__init__()
+        super(DominantChargeStatePredictorTorch, self).__init__()
 
         # tie the count of embeddings to the size of the vocabulary (count of amino acids)
         self.embeddings_count = len(alphabet)
@@ -57,46 +58,77 @@ class DominantChargeStatePredictor(tf.keras.Model):
         self.regressor_layer_size = regressor_layer_size
         self.recurrent_layers_sizes = recurrent_layers_sizes
 
-        self.embedding = tf.keras.layers.Embedding(
-            input_dim=self.embeddings_count,
-            output_dim=embedding_output_dim,
-            input_length=seq_length,
-        )
-        self._build_encoder()
-
-        self.attention = AttentionLayer()
-
-        self.regressor = tf.keras.Sequential(
-            [
-                tf.keras.layers.Dense(self.regressor_layer_size, activation="relu"),
-                tf.keras.layers.Dropout(rate=self.latent_dropout_rate),
-            ]
+        self.embedding = nn.Embedding(
+            num_embeddings=self.embeddings_count,
+            embedding_dim=embedding_output_dim,
+            padding_idx=0    # TODO check this
         )
 
-        self.output_layer = tf.keras.layers.Dense(num_classes, activation="softmax")
-
-    def _build_encoder(self):
-        self.encoder = tf.keras.Sequential(
-            [
-                tf.keras.layers.Bidirectional(
-                    tf.keras.layers.GRU(
-                        units=self.recurrent_layers_sizes[0], return_sequences=True
-                    )
-                ),
-                tf.keras.layers.Dropout(rate=self.dropout_rate),
-                tf.keras.layers.GRU(
-                    units=self.recurrent_layers_sizes[1], return_sequences=True
-                ),
-                tf.keras.layers.Dropout(rate=self.dropout_rate),
-            ]
+        # build encoder
+        self.encoder = nn.Sequential(
+            OrderedDict([
+                ("bidirectional_GRU", nn.GRU(   # TODO check the parameter values
+                    input_size=embedding_output_dim,
+                    hidden_size=self.recurrent_layers_sizes[0],
+                    batch_first=True,
+                    bidirectional=True,
+                )),
+                ("encoder_dropout1", nn.Dropout(self.dropout_rate)),
+                ("unidirectional_GRU", nn.GRU(   # TODO check the parameter values
+                    input_size=recurrent_layers_sizes[0]*2,
+                    hidden_size=self.recurrent_layers_sizes[1],
+                    batch_first=True,
+                    bidirectional=False,
+                )),
+                ("encoder_dropout2", nn.Dropout(self.dropout_rate)),
+            ])
+        )
+        
+        # instead of building a pytorch equivalent for the self-made tf AttentionLayer, use the build-in pytorch MultiheadAttention
+        self.attention = nn.MultiheadAttention(    # TODO check the parameter values
+            embed_dim=self.recurrent_layers_sizes[1],
+            num_heads=1,  # TODO think about increasing this
+            batch_first=True
         )
 
-    def call(self, inputs):
+        self.regressor = nn.Sequential(
+            OrderedDict([
+                ("dense", nn.Linear(
+                    in_features=self.recurrent_layers_sizes[1],
+                    out_features=self.regressor_layer_size,
+                )),
+                ("activation_relu", nn.ReLU()),
+                ("regressor_dropout", nn.Dropout(self.latent_dropout_rate))
+            ])
+        )
+
+        self.output_layer = nn.Linear(
+            in_features=self.regressor_layer_size,
+            out_features=num_classes
+        )
+
+        self.activation = nn.Softmax()
+
+    def forward(self, inputs):
+        """
+        Forward pass of the model.
+
+        Parameters
+        ----------
+        x : tensor
+            Input tensor (shape: [batch_size, seq_length]).
+
+        Returns
+        -------
+        tensor
+            Predicted output (shape: [batch_size, num_classes]).
+        """
         x = self.embedding(inputs)
-        x = self.encoder(x)
+        x, _ = self.encoder(x)
         x = self.attention(x)
         x = self.regressor(x)
         x = self.output_layer(x)
+        x = self.activation(x)
         return x
 
 
