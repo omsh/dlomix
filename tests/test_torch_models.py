8613a33d934832157e48d68e87eb4973c8277048
ayla-s <evaaylaschroeder@gmail.com>
2025-02-05 14:30:58 +0000

    started preparing the tests for torch

---
+++ tests/test_torch_models.py
@@ -0,0 +1,19 @@
+import logging
+
+from dlomix.models import DominantChargeStatePredictorTorch
+
+logger = logging.getLogger(__name__)
+
+
+def test_dominant_chargestate_model_torch():
+    model = DominantChargeStatePredictorTorch()
+    logger.info(model)
+    assert model is not None
+
+    #logger.info()
+
+    # check model.parameters not empty --> nn.Module.parameters()
+
+# to compare tf & torch: shapes at beginnin & end of forward & in between the layers - compare
+
+# test in run script: simple data --> fit & compare outputs
