4287dcaa50b1cf7f81187964353226eeebfcbbe0
ayla-s <evaaylaschroeder@gmail.com>
2025-02-06 10:53:07 +0000

    made relative CSD and major CS run scripts more compatible

--- run_scripts/run_prosit_chargestate_distribution.py
+++ run_scripts/run_prosit_chargestate_distribution.py
@@ -1,18 +1,16 @@
-import keras
 import tensorflow as tf
 
 from dlomix.constants import PTMS_ALPHABET
 from dlomix.data import ChargeStateDataset
 from dlomix.eval import adjusted_mean_absolute_error
-from dlomix.models import ChargeStateDistributionPredictor
+from dlomix.models import ChargeStatePredictor
 
-# Model
 
-model = model = ChargeStateDistributionPredictor(
-    num_classes=6, seq_length=32, alphabet=PTMS_ALPHABET
+model = model = ChargeStatePredictor(
+    num_classes=6, seq_length=32, alphabet=PTMS_ALPHABET, model_flavour="relative"
 )
-
 print(model)
+
 
 optimizer = tf.keras.optimizers.Adam(lr=0.0001)
 
@@ -24,7 +22,6 @@ d = ChargeStateDataset(
     max_seq_len=30,
     batch_size=512,
 )
-
 print(d)
 
 for x in d.tensor_train_data:
@@ -35,19 +32,18 @@ test_targets = d["test"]["charge_state_dist"]
 test_sequences = d["test"]["modified_sequence"]
 
 # callbacks
-
-weights_file = "./output/prosit_charge_dist_ms1_test"
+weights_file = "./output/prosit_charge_dist_test"
 checkpoint = tf.keras.callbacks.ModelCheckpoint(
     weights_file, save_best_only=True, save_weights_only=True
 )
+early_stop = tf.keras.callbacks.EarlyStopping(patience=20)
+callbacks = [checkpoint, early_stop]
 
-stop = keras.callbacks.EarlyStopping(patience=20)
 
-callbacks = [checkpoint, stop]
+model.compile(
+    optimizer=optimizer, loss="mean_squared_error", metrics=[adjusted_mean_absolute_error]
+)
 
-metrics = [adjusted_mean_absolute_error]
-
-model.compile(optimizer=optimizer, metrics=metrics, loss="mean_squared_error")
 
 history = model.fit(
     d.tensor_train_data,
